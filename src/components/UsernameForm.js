03398fe8dbe096ba44deee38b92b57206df73745
unknown <nbatre1@yahoo.com>
2018-12-29 11:33:33 -0600

    keep moving forward

--- src/components/UsernameForm.js
+++ src/components/UsernameForm.js
@@ -1,3 +1,41 @@
 
 //import react
 import React from 'react'
+
+class UsernameForm extends React.Component{
+   constructor(props){
+       super(props)
+       this.state = {
+            username:''
+       }
+       /// Binds both of the functions to the constructor and this
+     this.onChange = this.onChange.bind(this);
+     this.onSubmit = this.onSubmit.bind(this);
+   }
+   onChange(e) {
+    this.setState({
+        username: e.target.value,
+    })
+}
+
+  onSubmit (e) {
+      e.preventDefault()
+      this.props.onSubmit(this.state.username)
+  }
+
+   // render function that returns things
+
+   render(){
+       return <div>
+           <form onSubmit ={this.onSubmit}>
+               <input type ="text" placeholder="What is your username?"  onChange ={this.onChange}/>
+               <input type ="submit" />
+           </form>
+       </div>
+   }
+
+
+
+}
+
+export default UsernameForm
