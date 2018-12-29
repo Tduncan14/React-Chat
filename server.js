7b9f392f41eb753920719ebe5847a9a23285712e
unknown <nbatre1@yahoo.com>
2018-12-29 12:51:58 -0600

    keep moving forward

--- server.js
+++ server.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const Chatkit = require('pusher-chatkit-server');
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
+// creating on local server chatkit an instance of chatkit
+
+const chatkit = new Chatkit.default({
+  instanceLocator:'v1:us1:3048226e-2812-45f3-89b5-4ea7aaa1b954',
+  key:'a62541d2-d55d-43cd-acb1-a548dc2c5303:gWamm8SQNP6lT1L8isnhFja/H/dv7g6QZSSefsLpE10='
+})
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/users', (req,res) => {
+  const {username} = req.body
+
+  // creatimg the user
+  chatkit
+   .createUser({
+     name: username,
+     id: username
+   })
+    .then(() => res.sendStatus(201))
+    .catch(error =>{
+      if(error.error_type ==='services/chatkit/user_already_exists'){
+        res.sendStatus(200)
+      }
+      else{
+        res.status(error.statusCode).json(error)
+      }
+    });
+});
 
 const PORT = 3001
 app.listen(PORT, err => {
@@ -15,4 +44,4 @@ app.listen(PORT, err => {
   } else {
     console.log(`Running on port ${PORT}`)
   }
-})
+});
--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 import UsernameForm from './components/UsernameForm'
 
 class App extends Component {
+  constructor(){
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
+  }
+  onUsernameSubmitted(username){
+
+
+  }
+
   render() {
-    return <UsernameForm onSubmit = {username => alert(username)}/>
+
+    return <UsernameForm onSubmit = {username =>{this.onUsernameSubmitted}}
+    />
   }
 }
 
