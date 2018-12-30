c3b85805e5f1d9a5c67bcca8050f273f3cb5e875
unknown <nbatre1@yahoo.com>
2018-12-29 19:55:25 -0600

    keep moving forward

--- src/ChatScreen.js
+++ src/ChatScreen.js
@@ -39,8 +39,16 @@ class ChatScreen extends Component {
         flexDirection:'column'
       },
       chatContainer:{
-        
-      }
+          display: 'flex',
+          flex:1,
+      },
+       whosOnlineListContainer:{
+         width: '300px',
+         flex:'none',
+         padding: 20,
+         backgroundColor:'#2c303b',
+         color:'White',
+       },
     }
   }
 }
